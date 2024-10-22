deeea9720a022a10c6cbbaf3cb71430f9177cda8
Kristy Fournier <124598538+kristy-fournier@users.noreply.github.com>
2024-10-22 00:01:18 -0400

    you know breezy stuff

--- RaspberryPI/startup.py
+++ RaspberryPI/startup.py
@@ -1,1 +1,12 @@
-import threading
+"""
+Assume that this file is being run at startup, and should do all the things needed
+    ()  Check USB for new mp3's
+    ()  Check USB for updated data files (api keys, ports, )
+    ()  Copy them to correct directory
+    ()  Make sure the audio device is set to the 3.5mm jack
+    ()  Run database updater if new files were found
+    ()  Run Server
+    ()  Run http module to host client
+    ()  Generate a qr code and display it somehow (maybe)
+"""
+import threading, os, shutil, 
